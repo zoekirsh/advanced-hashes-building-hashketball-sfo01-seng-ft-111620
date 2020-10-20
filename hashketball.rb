7c0bd166d537a6bccd1b5c20f5f143755a0e38d8
Zoe Kirsh <zoekirsh@gmail.com>
2020-10-20 00:25:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,18 @@
 def game_hash 
-  bball = {:home => {team_name: "Brooklyn Nets", colors: ["Black", "White"], players: [ {}, {},  ]},  
-  :away => {team_name: "Charlotte Hornets", colors: ["Turquoise", "Purple"], players: []}
+  bball = {:home => {team_name: "Brooklyn Nets", 
+  colors: ["Black", "White"], 
+  players: [{player_name: "", number: "", shoe: "", points: "", rebounds: "", assists: "", steals: "", blocks: "", slam_dunks: ""},
+  {player_name: "", number: "", shoe: "", points: "", rebounds: "", assists: "", steals: "", blocks: "", slam_dunks: ""},
+  {player_name: "", number: "", shoe: "", points: "", rebounds: "", assists: "", steals: "", blocks: "", slam_dunks: ""},
+  {player_name: "", number: "", shoe: "", points: "", rebounds: "", assists: "", steals: "", blocks: "", slam_dunks: ""},
+  {player_name: "", number: "", shoe: "", points: "", rebounds: "", assists: "", steals: "", blocks: "", slam_dunks: ""}], 
+  :away => {team_name: "Charlotte Hornets", 
+  colors: ["Turquoise", "Purple"], 
+  players: [{player_name: "", number: "", shoe: "", points: "", rebounds: "", assists: "", steals: "", blocks: "", slam_dunks: ""},
+  {player_name: "", number: "", shoe: "", points: "", rebounds: "", assists: "", steals: "", blocks: "", slam_dunks: ""},
+  {player_name: "", number: "", shoe: "", points: "", rebounds: "", assists: "", steals: "", blocks: "", slam_dunks: ""},
+  {player_name: "", number: "", shoe: "", points: "", rebounds: "", assists: "", steals: "", blocks: "", slam_dunks: ""},
+  {player_name: "", number: "", shoe: "", points: "", rebounds: "", assists: "", steals: "", blocks: "", slam_dunks: ""}] }
   }
  
   bball
