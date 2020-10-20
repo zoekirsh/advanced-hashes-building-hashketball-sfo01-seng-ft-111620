3390482e6f19509d4769d8dc83ba75011cca777a
Zoe Kirsh <zoekirsh@gmail.com>
2020-10-20 00:15:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,13 +1,6 @@
 def game_hash 
-  bball = {:home => {}, :away => {}}
-  
-  bball.each do |key, value|
-    value[:team_name] = ""
-    value[:colors] = []
-    value[:players] = []
-  end
-
-  bball[:home]
-  
+  bball = {:home => {}, 
+  :away => {}}
+ 
   bball
 end 
