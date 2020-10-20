925ed8e731df1b4e31eb65ac4045c620e54fefa9
Zoe Kirsh <zoekirsh@gmail.com>
2020-10-20 00:40:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,5 @@
 def game_hash 
-  bball = {:home => {team_name: "Brooklyn Nets", colors: [], players: [], :away => {team_name: "Charlotte "}}
+  bball = {:home => {team_name: "Brooklyn Nets", colors: [], players: [], :away => {team_name: "Charlotte Hornets"}}
 
   
   bball
