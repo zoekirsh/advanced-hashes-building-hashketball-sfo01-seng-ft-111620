e2774a51faf1c585795397f873ad450c881b736e
Zoe Kirsh <zoekirsh@gmail.com>
2020-10-20 00:26:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,7 @@
 def game_hash 
   bball = {:home => {team_name: "Brooklyn Nets", 
   colors: ["Black", "White"], 
-  players: [{player_name: "", number: "", shoe: "", points: "", rebounds: "", assists: "", steals: "", blocks: "", slam_dunks: ""},
+  players: [{player_name: "Alan Anderson", number: "0", shoe: "16", points: "22", rebounds: "12", assists: "12", steals: "3", blocks: "1", slam_dunks: "1"},
   {player_name: "", number: "", shoe: "", points: "", rebounds: "", assists: "", steals: "", blocks: "", slam_dunks: ""},
   {player_name: "", number: "", shoe: "", points: "", rebounds: "", assists: "", steals: "", blocks: "", slam_dunks: ""},
   {player_name: "", number: "", shoe: "", points: "", rebounds: "", assists: "", steals: "", blocks: "", slam_dunks: ""},
