9fad347edacccbd9ba71b61ca52c9688cbb88f76
Zoe Kirsh <zoekirsh@gmail.com>
2020-10-20 00:29:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,7 @@ def game_hash
   colors: ["Black", "White"], 
   players: [{player_name: "Alan Anderson", number: "0", shoe: "16", points: "22", rebounds: "12", assists: "12", steals: "3", blocks: "1", slam_dunks: "1"},
   {player_name: "Reggie Evans", number: "30", shoe: "14", points: "12", rebounds: "12", assists: "12", steals: "12", blocks: "12", slam_dunks: "7"},
-  {player_name: "", number: "", shoe: "", points: "", rebounds: "", assists: "", steals: "", blocks: "", slam_dunks: ""},
+  {player_name: "Brook Lopez", number: "11", shoe: "17", points: "17", rebounds: "19", assists: "10", steals: "3", blocks: "1", slam_dunks: "15"},
   {player_name: "", number: "", shoe: "", points: "", rebounds: "", assists: "", steals: "", blocks: "", slam_dunks: ""},
   {player_name: "", number: "", shoe: "", points: "", rebounds: "", assists: "", steals: "", blocks: "", slam_dunks: ""}], 
   :away => {team_name: "Charlotte Hornets", 
